0857cbb3d07dac2b305acbd074b9652cf57efe06
MShahbazh <msh23904t@gmail.com>
2024-10-05 16:52:02 +0500

    Update Problem 1.c

--- Lab Assignments/Lab 06 (Loops)/Problem 1.c
+++ Lab Assignments/Lab 06 (Loops)/Problem 1.c
@@ -1,7 +1,5 @@
 /*Problem Statement: 
-Which loop system would be better for user input? Justify your answer by creating a program
-that takes a value and adds it to a variable and prints it repeatedly until the user enters a zero
-value. */
+Which loop system would be better for user input? Justify your answer by creating a program that takes a value and adds it to a variable and prints it repeatedly until the user enters a zero value. */
 
 /*Answer: 
 All three loops can be used for user input but the do-while is more preferable as it can first take the user input (do) and then run a loop on the input according to the while condition, so it will run for atleast one time. On the other hand, the while and for loop first require a user value and a condition to run. */                                                 
